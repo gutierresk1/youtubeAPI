412ad5a108d5952f417f1141b868280dadf71be1
gutierresk1 <43971263+gutierresk1@users.noreply.github.com>
2021-02-10 19:29:02 -0500

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAwfD3KkxvjNihs6xz_eRN7J9y1pcrSPDo';
+const KEY = 'KEY was removed due to privacy issues';
 
 export default axios.create({
     baseURL : 'https://www.googleapis.com/youtube/v3',
